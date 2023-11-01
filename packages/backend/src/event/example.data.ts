d9ba27892733e141374f5ac7786f4cdae2c4a333
Tobiass Trienekens <tobias.trienekens@rwth-aachen.de>
2023-11-01 09:16:43 +0100

    chore: remove teamevent from test data

--- packages/backend/src/event/example.data.ts
+++ packages/backend/src/event/example.data.ts
@@ -2,19 +2,18 @@ import { Event } from './event.dto';
 import { v4 } from 'uuid';
 
 export const exampleData: Event[] = [
-  Event.create(v4(), 'Teamevent', new Date(), 'Cologne'),
   Event.create(
     v4(),
     'X-mas party',
     new Date('2023-12-22T20:00:00Z'),
     'Cologne',
   ),
-  Event.create(v4(), 'Carnival', new Date('2023-02-12T20:00:00Z'), 'Aachen'),
+  Event.create(v4(), 'Carnival', new Date('2023-02-12T20:00:00Z'), 'Cologne'),
   {
     id: v4(),
     title: "New Year's Eve",
     date: new Date('2023-12-31T22:00:00Z'),
-    city: 'Kerken',
+    city: 'Aachen',
     tickets: [
       {
         firstName: 'Cassidy',
